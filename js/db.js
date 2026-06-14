55719974817de0bb909f8408b16a70164c1467c3
alanmts-am <alanmts.am@gmail.com>
2026-06-14 15:42:53 -0300

    refactor: change environments

--- js/db.js
+++ js/db.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL  = "https://tcegnkvivvjzbkealeno.supabase.co";
-const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZWdua3ZpdnZqemJrZWFsZW5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODk2OTQsImV4cCI6MjA5NjM2NTY5NH0.DowowhInp7nxRrrw6FLTHXq-Kn3ftQTFIlFuVBodJnU";
+const SUPABASE_URL  = 'https://unrkcldkqrncejxsssui.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVucmtjbGRrcXJuY2VqeHNzc3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NDkyNjAsImV4cCI6MjA5NzAyNTI2MH0.rWVIQfPIzYnU4OGQ-0J2aohjcX1FPH2GC7zsWX3ffn0';
 
 export const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
